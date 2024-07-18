83d1b602e52060eee0b6714cf07e86f041784de9
ElkiIgolki <dianaderbina2@gmail.com>
2024-07-18 17:03:58 +0300

    Update hm.py

--- hm.py
+++ hm.py
@@ -1,21 +1,28 @@
 calls = 0
+
+
 def count_calls():
     global calls
     calls += 1
+    return calls
 
 
 def string_info(string):
     lowUp = (len(string), string.upper(), string.lower())
     print(lowUp)
     count_calls()
+    return lowUp
 
 
 def is_contains(string, list_to_search):
-    if string in list_to_search:
+    for i in range(len(list_to_search)):
+        list_to_search[i] = list_to_search[i].lower()
+    if string.lower() in list_to_search:
         print(True)
     else:
         print(False)
     count_calls()
+    return string, list_to_search
 
 
 string_info('До инфаркта')
